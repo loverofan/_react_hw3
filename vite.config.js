1603bf6c6707cc648b08d6f48a5cc7c8bbf866fa
Ed_Hsu <43668240+loverofan@users.noreply.github.com>
2023-09-06 00:02:36 +0800

    Add base to vite config file.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/_react_hw3/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/_react_hw3/' : '/',
   plugins: [react()],
 })
